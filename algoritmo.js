d5bb93d27bafe1f0a4113bcba2c842f06eb4e84c
Julen <platerojulen@gmail.com>
2021-01-24 19:46:56 +0100

    ultimo commit arreglando la descripción de mi teoría inicial

--- algoritmo.js
+++ algoritmo.js
@@ -1,11 +1,11 @@
 // Demuestra que la siguiente cadena 'geeksforgeeks' no es un palindromo.
-// Planteamiento: Es necesario crear una variable que contenga esa string. Aplicar algo para que nos de el texto reverso y con ello crear un array que recorra mediante bucle for
+// Planteamiento: Es necesario crear una variable que contenga esa string. Aplicar algo para que nos de el texto reverso 
 // y con ello tendremos que desarrollar un condicional if que registre si la variable de string y su reverso son lo mismo, si no, dará un error.
 
 function esPalindromo(str){
     //Info sacada de: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/String/split
     let strReverse = str.split('').reverse().join(''); // creamos el reverso del str.  
-        if (str === strReverse){
+        if (str === strReverse){ //aplicamos el condicional para que nos indique si es o no Palindromo 
             console.log('Es un palindromo')
         }
         else {
